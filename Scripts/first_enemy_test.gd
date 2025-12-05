540b6d3797ead6be73eb7a5a2704159e92cc2fec
Benjamin Feldgendler <benjaminfeldgendler@Benjamins-MacBook-Air.local>
2025-12-05 11:03:17 +0100

    adds initial first enemy node

--- Scripts/first_enemy_test.gd
+++ Scripts/first_enemy_test.gd
@@ -1,20 +1,16 @@
 extends CharacterBody2D
 
-@onready var bullet = preload("res://scripts/bullet.gd")
-
-var max_speed = 100
+var speed = 100
 var last_direction := Vector2(1,0)
 
+@onready var player: CharacterBody2D = $"."
 
-	
 func _physics_process(delta):
-	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = direction * max_speed
+	var direction = (player.position-position).normalized()
+	velocity = direction * speed
+	look_at(player.position)
 	move_and_slide()
-	if Input.is_action_just_pressed("ui_accept"):
-		var bullet_temp = bullet.instantiate()
-		bullet_temp.direction = 1
-		add_child (bullet_temp)
+	
 	
 	if direction.length() > 0:
 		last_direction = direction
