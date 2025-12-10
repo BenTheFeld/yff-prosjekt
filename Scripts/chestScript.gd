91868310db4e7bc790bd0dadbad8e88692726fcd
AlexStenshagen <alexstenshagen123@gmail.com>
2025-12-10 13:01:13 +0100

    Ny

--- Scripts/chestScript.gd
+++
@@ -1,5 +0,0 @@
-extends AnimatedSprite2D
-
-func interact():
-	play("chest_open")
-	set_process(false)  # Prevent re-triggering   
--- Scripts/player_movement.gd
+++ Scripts/player_movement.gd
@@ -1,106 +1,37 @@
 extends CharacterBody2D
 
+@onready var bullet = preload("res://bullet.gd")
+
 var max_speed = 100
-var last_direction := Vector2(1, 0)
-var number = 0
-
-@onready var bullet = preload("res://Scenes/bullet.tscn")
-
-# --- Shooting cooldown ---
-var can_shoot := true
-var shoot_cooldown := 1 	# seconds between shots
-
-# --- CHEST INTERACTION VARIABLES ---
-var current_interactable_chest: Area2D = null # Stores a reference to the chest the player is currently near
+var last_direction := Vector2(1,0)
 
 
-func shoot():
-	var bullet_temp = bullet.instantiate()
-
-	# Spawn bullet at player position (you can add an offset later)
-	bullet_temp.position = position
-
-	get_tree().get_root().add_child(bullet_temp)
-
-	# Reset bullet direction
-	bullet_temp.direction_x = 0
-	bullet_temp.direction_y = 0
-
-	# Mitt geni nummersystem
-	if number == 1:
-		bullet_temp.direction_x = 1
-	elif number == 2:
-		bullet_temp.direction_x = -1
-	elif number == 3:
-		bullet_temp.direction_y = 1
-	elif number == 4:
-		bullet_temp.direction_y = -1
-
-
-func start_shoot_cooldown():
-	can_shoot = false
-	await get_tree().create_timer(shoot_cooldown).timeout
-	can_shoot = true
-
-
-@warning_ignore("unused_parameter")
+	
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * max_speed
 	move_and_slide()
-
-	# --- Holding shoot button, with delay ---
-	if Input.is_action_pressed("ui_accept") and can_shoot:
-		shoot()
-		start_shoot_cooldown()
-
-	# --- CHEST INTERACTION LOGIC (Using a separate action for clarity, e.g., "interact") ---
-	# NOTE: You must set up a separate "interact" action in Project Settings -> Input Map (e.g., 'E' key)
-	if Input.is_action_just_pressed("interact"):
-		if current_interactable_chest != null:
-			# Call the public 'open' function on the chest script
-			current_interactable_chest.open()
-
-
-	# Movement + animation
+	if Input.is_action_just_pressed("ui_accept"):
+		var bullet_temp = bullet.instantiate()
+		bullet_temp.direction = 1
+		add_child (bullet_temp)
+	
 	if direction.length() > 0:
 		last_direction = direction
-		play_walk_animation(direction)
+		play_walk_animation (direction)
+	
 	else: 
-		play_idle_animation(last_direction)
-
-
-# --- CHEST INTERACTION FUNCTIONS (Called by the Chest's Area2D script) ---
-
-# Called by the Chest script when the player enters its area
-func set_interactable_chest(chest: Area2D):
-	current_interactable_chest = chest
-	# Add UI update logic (e.g., show "Press E to open")
-
-# Called by the Chest script when the player leaves its area
-func clear_interactable_chest():
-	current_interactable_chest = null
-	# Add UI update logic (e.g., hide "Press E to open")
-
-
-# --- Existing Animation Functions ---
-
-func play_walk_animation(direction):
+		play_idle_animation (last_direction)
+func play_walk_animation (direction):
 	if direction.x > 0:
 		$AnimatedSprite2D.play("walk_right")
-		number = 1
 	elif direction.x < 0:
 		$AnimatedSprite2D.play("walk_left")
-		number = 2
 	elif direction.y > 0:
 		$AnimatedSprite2D.play("walk_down")
-		number = 3
 	elif direction.y < 0:
 		$AnimatedSprite2D.play("walk_up")
-		number = 4
-
-
-func play_idle_animation(direction):
+func play_idle_animation (direction):
 	if direction.x > 0:
 		$AnimatedSprite2D.play("idle_right")
 	elif direction.x < 0:
@@ -109,14 +40,3 @@ func play_idle_animation(direction):
 		$AnimatedSprite2D.play("idle_down")
 	elif direction.y < 0:
 		$AnimatedSprite2D.play("idle_up")
-
-
-func play_shoot_animation(direction):
-	if direction.x > 0:
-		$AnimatedSprite2D.play("shoot_right") 
-	elif direction.x < 0:
-		$AnimatedSprite2D.play("shoot_left")
-	elif direction.y > 0:
-		$AnimatedSprite2D.play("shoot_down")
-	elif direction.y < 0:
-		$AnimatedSprite2D.play("shoot_up")
