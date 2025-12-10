ad5b9100131be928e3a98c2a3411913fe2ea2bae
AlexStenshagen <alexstenshagen123@gmail.com>
2025-12-10 10:20:00 +0100

    Chest

--- Chest.gd
+++ Chest.gd
@@ -1,6 +1,6 @@
 extends AnimatedSprite2D
 
-@onready var animations: AnimatedSprite2D = $AnimationPlayer
+@onready var animations: AnimatedSprite2D = $AnimatedSprite2D
 # Called when the node enters the scene tree for the first time.
 func interact() -> void:
 	animations.play("chest_open")
