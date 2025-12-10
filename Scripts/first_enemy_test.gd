344e9ab20df77c35b2560365e6e02d8445d1e6f4
BenTheFeld <benjamin.feldgendler@gmail.com>
2025-12-10 09:14:34 +0100

    adds movement to enemy

--- Scripts/first_enemy_test.gd
+++ Scripts/first_enemy_test.gd
@@ -1,31 +1,34 @@
 extends CharacterBody2D
 
-var speed = 100
+var speed = 20
 var last_direction := Vector2(1,0)
+var direction_x = 0
+var direction_y = 0
+var player_position
+var target_position
 
-@onready var player: CharacterBody2D = $"."
+@onready var player: CharacterBody2D = $"../Player"
 
 func _physics_process(delta):
-	var direction = (player.position-position).normalized()
-	velocity = direction * speed
-	look_at(player.position)
-	move_and_slide()
+	print(player.position - position)
+	player_position = player.position
+	target_position = (player.position - position).normalized()
+	velocity = target_position * speed
+	if position.distance_to(player.position) > 3:
+		move_and_slide()
+		
+	play_walk_animation (direction_x, direction_y)
 	
-	
-	if direction.length() > 0:
-		last_direction = direction
-		play_walk_animation (direction)
-	
-	else: 
-		play_idle_animation (last_direction)
-func play_walk_animation (direction):
-	if direction.x > 0:
+#	else: 
+#	play_idle_animation (last_direction)
+func play_walk_animation (direction_x, direction_y):
+	if direction_x > 0:
 		$AnimatedSprite2D.play("walk_right")
-	elif direction.x < 0:
+	elif direction_x < 0:
 		$AnimatedSprite2D.play("walk_left")
-	elif direction.y > 0:
+	elif direction_y > 0:
 		$AnimatedSprite2D.play("walk_down")
-	elif direction.y < 0:
+	elif direction_y < 0:
 		$AnimatedSprite2D.play("walk_up")
 func play_idle_animation (direction):
 	if direction.x > 0:
